3bcba885b5e05895216abe53530d08fe69af047c
Prayas-Rathore <prayas.rathore@growwstacks.com>
2026-02-10 13:10:39 +0530

    Revert "merge resolved"
    This reverts commit 492a85a17bae913f8550c876afe6d4a2942eca73.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,        // exposes network automatically
-    port: 5173
-  }
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
